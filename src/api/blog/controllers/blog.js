d82ae6c6c8afa2832297c29dd33a1f56ec377c5b
yaserRayhan <rayhanyaser1@gmail.com>
2022-11-20 09:45:04 +0700

    feat:next and previous blog in detail

--- src/api/blog/controllers/blog.js
+++ src/api/blog/controllers/blog.js
@@ -22,6 +22,29 @@ module.exports = createCoreController("api::blog.blog", ({ strapi }) => ({
       .service("api::blog.blog")
       .findOne(blogs[0]?.id, query);
 
+    //get the next blog by id
+    const nextBlog = await strapi.entityService.findMany("api::blog.blog", {
+      filters: {
+        id: {
+          $gt: blogs[0]?.id,
+        },
+      },
+      sort: "id:asc",
+      limit: 1,
+    });
+
+    //get the previous blog by id
+    const previousBlog = await strapi.entityService.findMany("api::blog.blog", {
+      filters: {
+        id: { $lt: blogs[0]?.id },
+      },
+      sort: "id:desc",
+      limit: 1,
+    });
+
+    entity.nextBlog = nextBlog[0] || null;
+    entity.previousBlog = previousBlog[0] || null;
+
     // const entity = await  await super.findOne(ctx);
     const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
 
